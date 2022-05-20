3c7b75c13ffdbc155f5f5e825bb92c9826202d2f
Andrew Lehmer <andrew.lehmer@gmail.com>
2022-05-20 15:34:02 -0400

    Improve trie compression
    Adjusted the trie compression algorithm to also consider chains that do
    necessarily terminate at leaf nodes. With formatting, this reduced the
    file size of wordlyzer.html by over 14 KiB. The minimal JSON
    encoding was reduced by a bit over 600 B.

--- triegen.py
+++ triegen.py
@@ -30,32 +30,32 @@ def generate_trie(words: Iterable[str]) -> TrieNode:
       node = node[letter]
   return trie
 
-def compress_trie_recursive(node: TrieNode) -> str:
+def compress_trie_recursive(node: TrieNode) -> tuple[str, TrieNode]:
   """
-  Recursively compresses a character-wise trie such that branches consisting of
-  a chain of nodes with one or zero children are converted into a leaf node
-  whose key is the ordered concatenation of all of the keys in the original
-  chain. Returns a non-empty string if the node is part of such a chain.
+  Recursively compresses a character-wise trie such that chains of nodes with
+  one child are converted into a single node whose key is the ordered
+  concatenation of all of the keys in the original chain. Returns a 2-tuple
+  containing:
+    1. a string representing the chain path originating at the given node
+    2. the node at the end of the chain
+
+  If the node argument does not have exactly one child, (1) is the empty
+  string (""), and (2) is the original node argument, potentially modified.
   """
   if len(node) == 1:
-    letter: str
-    child: TrieNode
     letter, child = next(iter(node.items()))
-    if child:
-      if suffix := compress_trie_recursive(child):
-        return letter + suffix
-      # The child had multiple children.
-      return ""
-    return letter
-  # Save aside suffix replacements to avoid mutation during iteration.
-  suffixes: dict[str, str] = {}
+    path, end = compress_trie_recursive(child)
+    return letter + path, end
+  # Avoid mutation during iteration.
+  replacements: dict[str, tuple[str, TrieNode]] = {}
   for letter, child in node.items():
-    if child and (suffix := compress_trie_recursive(child)):
-      suffixes[letter] = letter + suffix
-  for letter, suffix in suffixes.items():
+    path, end = compress_trie_recursive(child)
+    if path:
+      replacements[letter] = (letter + path, end)
+  for letter, (path, child) in replacements.items():
     del node[letter]
-    node[suffix] = TrieNode()
-  return ""
+    node[path] = child
+  return "", node
 
 def compress_trie(trie: TrieNode) -> TrieNode:
   """
